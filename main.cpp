cc10e5c251eef5da9fcbd0f2887bc3d875588400
wjy <2899340@qq.com>
2020-12-26 21:49:08 +0800

    asyncEchoServer

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <asio.hpp>
 
-int main(int argc, char** argv)
-{
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include "testEchoServerClass.h"
 
+int testCaseNumber = 0;
+
+int main(int argc, char* argv[])
+{
+    if(testCaseNumber ==0)
+    {
+        try
+        {
+            asio::io_context io_context;
+            AsyncEchoServer s(io_context, 8899);
+            io_context.run();
+        }
+        catch (std::exception& e)
+        {
+            std::cerr << "Exception: " << e.what() << "\n";
+        }
+    }
+    else if (testCaseNumber == 1)
+    {
+
+    }
+
+
+    return 0;
 }
---
+++ testEchoServerClass.h
@@ -0,0 +1,87 @@
+#include <cstdlib>
+#include <iostream>
+#include <memory>
+#include <utility>
+#include "asio.hpp"
+
+using asio::ip::tcp;
+
+/*
+当类A被share_ptr管理，且在类A的成员函数里需要把当前类对象作为参数传给其他函数时，
+就需要传递一个指向自身的share_ptr。
+*/
+class AsyncEchoServerSession
+    : public std::enable_shared_from_this<AsyncEchoServerSession>
+{
+public:
+    AsyncEchoServerSession(tcp::socket socket)
+        : socket_(std::move(socket))
+    {
+    }
+
+    void start()
+    {
+        do_read();
+    }
+
+private:
+    void do_read()
+    {
+        auto self(shared_from_this());
+        socket_.async_read_some(asio::buffer(data_, max_length),
+            [this, self](std::error_code ec, std::size_t length)
+            {
+                if (!ec)
+                {
+                    do_write(length);
+                }
+            });
+    }
+
+    void do_write(std::size_t length)
+    {
+        auto self(shared_from_this());
+        asio::async_write(socket_, asio::buffer(data_, length),
+            [this, self](std::error_code ec, std::size_t /*length*/)
+            {
+                if (!ec)
+                {
+                    do_read();
+                }
+            });
+    }
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class AsyncEchoServer
+{
+public:
+    AsyncEchoServer(asio::io_context& io_context, short port)
+        : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+    {
+        do_accept();
+    }
+
+private:
+    void do_accept()
+    {
+        //设置acceptor接收一个客户端的回调
+        acceptor_.async_accept(
+            [this](std::error_code ec, tcp::socket socket)
+            {
+                if (!ec)
+                {
+                    std::make_shared<AsyncEchoServerSession>(std::move(socket))->start();
+                }
+
+                do_accept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+};
+
+
